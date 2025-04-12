8ade9439a97f72b2f7e3c62f298c98b54f35cf5e
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2025-04-12 23:03:46 +0530

    chore: setup JSON Server with quiz question dataset

--- src/components/quiz/Quiz.tsx
+++ src/components/quiz/Quiz.tsx
@@ -1,7 +1,31 @@
-import { ChevronRight } from "lucide-react";
+import axios from "axios";
 import { Button } from "../ui/button";
+import { ChevronRight } from "lucide-react";
+import { useEffect, useState } from "react";
+import { IQuestion } from "@/types/QuizData";
 
 const Quiz = () => {
+  const [questionData, setQuestionData] = useState<IQuestion | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchQuestion = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/data");
+        const lastQuestion = response.data?.questions[9];
+        setQuestionData(lastQuestion);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching question: ", error);
+      }
+    };
+    fetchQuestion();
+  }, []);
+
+  if (loading || !questionData) {
+    return <div className="text-center p-4">Loading...</div>;
+  }
+
   return (
     <div className="mx-30 px-15 py-5 bg-white border border-gray-300 rounded-lg flex flex-col gap-14">
       {/* UpperPart  */}
@@ -18,21 +42,16 @@ const Quiz = () => {
 
       <div>
         <h2 className="text-2xl text-gray-900 px-15 leading-14">
-          The _____________ implementation of smart city technologies
-          _____________ urban efficiency and sustainability, _____________
-          quality of life for residents and _____________ a model for future
-          urban development.
+          {questionData.question}
         </h2>
       </div>
 
-      <div className="flex justify-center gap-4">
-        {["Enhancing", "Improved", "Providing", "Widespread"].map(
-          (text, index) => (
-            <Button variant={"outline"} key={index} className="cursor-pointer">
-              {text}
-            </Button>
-          )
-        )}
+      <div className="flex justify-center gap-4 flex-wrap">
+        {questionData.options.map((text: string, index: number) => (
+          <Button variant={"outline"} key={index} className="cursor-pointer">
+            {text}
+          </Button>
+        ))}
       </div>
 
       <div className="flex justify-end w-full">
