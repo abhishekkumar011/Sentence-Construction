2192d7935ecc19f9d4d5d5606793cd3e6ab79123
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2025-04-14 14:10:08 +0530

    rfc: refactor some code

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,4 @@ import Result from "./result/Result";
 import AnswerCard from "./answer-card/AnswerCard";
 import HeroSection from "./hero-section/HeroSection";
 
-export { Quiz, Result, HeroSection, AnswerCard };
+export { Quiz, HeroSection, AnswerCard, Result };
